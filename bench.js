d46f290e085ee83fb4ebb3c32bf932620013624e
Corex <adic3x@gmail.com>
2025-11-09 20:20:02 +0200

    Add array-based functions for cycle, of, and iter; update benchmarks

--- bench.js
+++ bench.js
@@ -3,6 +3,7 @@ const wasm = require('./pkg/rs');
 const I = 1_000;
 const N = 1_000_000;
 const A = Uint32Array.from({ length: N }, (_, i) => i !== N - 1 ? i % 256 : 512);
+const V = Array.from(A);
 
 function cycle(data, a) {
     for (let i = 0; i < data.length; i++) {
@@ -22,15 +23,45 @@ function iter(data, a) {
     return data.some(v => v >= a);
 }
 
+function array_cycle(data, a) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] >= a) return true;
+    }
+    return false;
+}
+
+function array_of(data, a) {
+    for (const i of data) {
+        if (i >= a) return true;
+    }
+    return false;
+}
+
+function array_iter(data, a) {
+    return data.some(v => v >= a);
+}
+
 console.log(
     (
         cycle(A, 400) &&
         of(A, 400) &&
         iter(A, 400) &&
+
+        array_cycle(V, 400) &&
+        array_of(V, 400) &&
+        array_iter(V, 400) &&
+
         wasm.cycle(A, 400) &&
         wasm.iter(A, 400) &&
+
         !cycle(A, 800) &&
+        !of(A, 800) &&
         !iter(A, 800) &&
+
+        !array_cycle(V, 800) &&
+        !array_of(V, 800) &&
+        !array_iter(V, 800) &&
+    
         !wasm.cycle(A, 800) &&
         !wasm.iter(A, 800)
     ) ? 'testing: ok' : 'testing: fail'
@@ -40,6 +71,11 @@ for (let i = 0; i < 1000; i++) {
     cycle(A, 400);
     of(A, 400);
     iter(A, 400);
+
+    array_cycle(V, 400);
+    array_of(V, 400);
+    array_iter(V, 400);
+
     wasm.cycle(A, 400);
     wasm.iter(A, 400);
 }
@@ -62,6 +98,21 @@ start = performance.now();
 for (let i = 0; i < I; i++) iter(A, 400);
 end = performance.now();
 console.log(`js iter ${((end - start) / I).toFixed(2)} ms`);
+
+start = performance.now();
+for (let i = 0; i < I; i++) array_cycle(V, 400);
+end = performance.now();
+console.log(`js non-typed cycle ${((end - start) / I).toFixed(2)} ms`);
+
+start = performance.now();
+for (let i = 0; i < I; i++) array_of(V, 400);
+end = performance.now();
+console.log(`js non-typed of ${((end - start) / I).toFixed(2)} ms`);
+
+start = performance.now();
+for (let i = 0; i < I; i++) array_iter(V, 400);
+end = performance.now();
+console.log(`js non-typed iter ${((end - start) / I).toFixed(2)} ms`);
 
 start = performance.now();
 for (let i = 0; i < I; i++) wasm.cycle(A, 400);
